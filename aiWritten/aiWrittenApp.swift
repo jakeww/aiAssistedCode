8b76eb5ac3e9da4bc206a770898d5849e113cfac
Jake Watembach <jakewatembach@gmail.com>
2022-12-06 11:06:58 -0700

    Initial Commit

---
+++ aiWritten/aiWrittenApp.swift
@@ -0,0 +1,17 @@
+//
+//  aiWrittenApp.swift
+//  aiWritten
+//
+//  Created by Jake Watembach on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct aiWrittenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
