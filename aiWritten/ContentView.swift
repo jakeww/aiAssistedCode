2fd9764aa8be6075c380b8c4ff380ee01908478d
Jake Watembach <jakewatembach@gmail.com>
2022-12-12 16:22:30 -0700

    paste an entire book at once

--- aiWritten/ContentView.swift
+++ aiWritten/ContentView.swift
@@ -8,23 +8,25 @@ struct ContentView: View {
     // Create a state variable to store the rotation angle
     @State private var rotation: Double = 0
 
-    //array of book pages
-    let pages = [
-        "Page 1: Lorem ipsum",
-        "Page 2: Vestibulum ante",
-        "Page 3: Pellentesque habitant",
-        "Page 4: Suspendisse potenti"
-    ]
-    
+    let bookText = """
+    Page one
+    Page two
+    Page three
+    """
+
+    // Use a computed property to store the pages in the book
+    var pages: [String] {
+        return bookText.components(separatedBy: "\n")
+    }
+
     var body: some View {
         VStack {
             Spacer()
             ZStack {
                 Color.white
-                
                 Text(pages[currentPage])
                     .padding()
-                    .font(.system(.title, design: .serif))
+                    .font(.system(.body, design: .serif))
                     .foregroundColor(.black)
                     .transition(.opacity) // add a transition
                     .rotation3DEffect(Angle(degrees: rotation), axis: (x: 0, y: 1, z: 0)) // apply the 3D rotation
@@ -42,14 +44,14 @@ struct ContentView: View {
                                 if value.translation.width < 0 {
                                     // Go to the next page if not already at the last page
                                     if self.currentPage < self.pages.count - 1 {
-                                        withAnimation(.easeInOut) {
+                                        withAnimation(.easeInOut(duration: 0)) {
                                             self.currentPage += 1
                                         }
                                     }
                                 } else if value.translation.width > 0 {
                                     // Go to the previous page if not already at the first page
                                     if self.currentPage > 0 {
-                                        withAnimation(.easeInOut) {
+                                        withAnimation(.easeInOut(duration: 0)) {
                                             self.currentPage -= 1
                                         }
                                     }
@@ -61,9 +63,6 @@ struct ContentView: View {
                                 }
                             }
                     )
-
-
-
             }
         }
     }
